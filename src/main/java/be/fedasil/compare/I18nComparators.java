317cbba5915f437adba02926f60b6af1bf4b585e
Elias Khan <khan.elias@gmail.com>
2021-01-12 19:48:43 +0100

    make i18n comparators return a map instead of lists

--- src/main/java/be/fedasil/compare/I18nComparators.java
+++ src/main/java/be/fedasil/compare/I18nComparators.java
@@ -4,23 +4,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.util.Map.*;
 import static org.apache.commons.lang3.BooleanUtils.isFalse;
 
 public class I18nComparators {
 	
 	private I18nComparators() { }
 	
-	public static List<Map.Entry<String, String>> getUntranslatedEntries(Map<String, String> m) {
+	public static Map<String, String> getUntranslatedEntries(Map<String, String> m) {
 		return m.entrySet()
 				.stream()
 				.filter(entry -> entry.getValue().matches("(NL_|FR_|NL -|FR -)\\s*\\w*"))
-				.collect(Collectors.toList());
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
 	}
 	
-	public static List<String> getKeysMissingFromOther(Map<String, String> a, Map<String, String> other) {
-		return a.keySet()
+	public static Map<String, String> getKeysMissingFromOther(Map<String, String> a, Map<String, String> other) {
+		return a.entrySet()
 				.stream()
-				.filter(keyA -> isFalse(other.containsKey(keyA)))
-				.collect(Collectors.toList());
+				.filter(entryA -> isFalse(other.containsKey(entryA.getKey())))
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
 	}
 }
