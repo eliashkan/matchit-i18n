a6f88591507a9fa67ade85a60a8d8c9d6c4fcd68
Elias Khan <khan.elias@gmail.com>
2021-01-12 19:25:59 +0100

    renamed to I18nComparators, misc upgrades

--- src/main/java/be/fedasil/compare/I18nComparators.java
+++ src/main/java/be/fedasil/compare/I18nComparators.java
@@ -6,21 +6,21 @@ import java.util.stream.Collectors;
 
 import static org.apache.commons.lang3.BooleanUtils.isFalse;
 
-public class CompareProperties {
+public class I18nComparators {
 	
-	private CompareProperties() { }
+	private I18nComparators() { }
 	
 	public static List<Map.Entry<String, String>> getUntranslatedEntries(Map<String, String> m) {
 		return m.entrySet()
 				.stream()
-				.filter(entry -> entry.getValue().matches("(NL_|FR_)\\w*"))
+				.filter(entry -> entry.getValue().matches("(NL_|FR_|NL -|FR -)\\s*\\w*"))
 				.collect(Collectors.toList());
 	}
 	
-	public static List<String> getMissingKeys(Map<String, String> a, Map<String, String> b) {
+	public static List<String> getKeysMissingFromOther(Map<String, String> a, Map<String, String> other) {
 		return a.keySet()
 				.stream()
-				.filter(keyA -> isFalse(b.containsKey(keyA)))
+				.filter(keyA -> isFalse(other.containsKey(keyA)))
 				.collect(Collectors.toList());
 	}
 }
