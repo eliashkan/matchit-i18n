9fbeac5df7678f9182c0fc582bcd668a4425f700
Elias Khan <khan.elias@gmail.com>
2021-01-12 19:51:44 +0100

    worksheetgenerator functional interface right parameters

--- src/main/java/be/fedasil/excel/WorksheetGenerator.java
+++ src/main/java/be/fedasil/excel/WorksheetGenerator.java
@@ -1,11 +1,10 @@
 package be.fedasil.excel;
 
-import java.util.List;
 import java.util.Map;
 
 @FunctionalInterface
 interface WorksheetGenerator {
-
-	void generate(List<Map.Entry<String, String>> entries);
-
+	
+	void generate(Map<String, String> map);
+	
 }
